c20712370be513e0be8984b79e5a7e48e69839d2
Srishan Bangera <srishanbangera@gmail.com>
2026-09-15 15:22:43 +0530

    Sets up image classification model
    - Retrieves and persists dataset class names to support model output mapping.
    - Implements data pipeline optimizations with prefetching for faster training.
    - Introduces data augmentation techniques to enhance model generalization.
    - Establishes a Convolutional Neural Network (CNN) architecture for image classification.

--- train_model.py
+++ train_model.py
@@ -36,7 +36,63 @@ validation_dataset = tf.keras.utils.image_dataset_from_directory(
 
 )
 
+class_name = train_dataset.class_names
 
+for index, class_name in enumerate(class_name):
 
+    print(
+        index,
+        ":",
+        class_name
+    )
 
+    num_classes = len(class_name)
+
+    if num_classes < 2:
+        print("Error: Only on class was detected")
+
+        print("Check your dataset folder structure.")
+
+        exit()
+
+with open(
+    "model/class_names.json",
+    "w"
+) as file:
+    json.dump(class_name, 
+              file indent = 4)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(
+    buffer_szie = AUTOTUNE
+)
+
+validation_dataset = validation_dataset.prefetch(
+    buffer_size = AUTOTUNE
+)
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1)
+    ]
+)
+
+model = keras.Sequential([
+
+    layers.Input(Shape = (128, 128, 3)),
+    data_augmentation,
+    layers.Rescaling(1./255),
+    layers.Convo2D(32, (3, 3), activation = 'relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Convo2D(64, (3, 3), activation = 'relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Flatten(),
+    layers.Dense(128, activation = 'relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_classes, activation = 'softmax')
+
+])
 
